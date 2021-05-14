7da74ec471629c83540a9f5932d9cb35c2197180
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-05-14 16:40:55 +0200

    Backend

--- Server/routes/api/movies.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Load movie model
-const Movie = require('../../models/Movie');
-
-// @route GET api/movies/
-// @description 
-// @access Public
