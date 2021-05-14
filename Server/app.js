5c8779c67e4ab6a443b2421baddcee6cb72430de
Arsen Niyonzima <arse0012@edu.easj.dk>
2021-05-14 18:08:51 +0200

    front- and backend connected

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books');
 
 const app = express();
 
 // Tilslut til databasen
 connectDB();
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send ('Her starter det hele'));
+
+// use Routes
+app.use('/api/books', books);
 
 const port = process.env.PORT || 8091;
 
